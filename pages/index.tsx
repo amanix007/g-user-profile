54719dce0b54e53aa747cd4361d72924a0890bed
Aman Ullah <amanix007@gmail.com>
2022-07-12 15:49:46 +0600

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+
+export default function PersistentDrawerLeft() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, ad.
+      </p>
+    </Box>
+  )
+}
