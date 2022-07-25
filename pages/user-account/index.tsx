118adbcff2f44593c5d0a556ad73695c13c35460
Aman Ullah <amanix007@gmail.com>
2022-07-25 19:07:15 +0600

    Calendar added

--- pages/user-account/index.tsx
+++ pages/user-account/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import TextField from "@mui/material/TextField";
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 
 import { Avatar, Button, Typography } from "@mui/material";
 import { Formik } from "formik";
@@ -11,6 +12,7 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { FieldArray } from "formik";
 import { Field } from "formik";
+import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 
 const Item = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -20,8 +22,14 @@ const Item = styled(Box)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 export default function UserAccount(props) {
+  const companyLogoRef = useRef(null);
 
-  console.log('props:', props)
+  const triggerFileUpload = () => {
+
+    console.log('companyLogoRef:', companyLogoRef)
+    companyLogoRef.current.click();
+  }
+
   const [state, setState] = useState({
     status_message: {
       type: "success",
@@ -30,7 +38,13 @@ export default function UserAccount(props) {
   });
 
   const { status_message } = state;
+  const [value, setValue] = React.useState<Date | null>(
+    new Date('2014-08-18T21:11:54'),
+  );
 
+  const handleChange = (newValue: Date | null) => {
+    setValue(newValue);
+  };
   return <div>
 
     <Formik
@@ -79,154 +93,174 @@ export default function UserAccount(props) {
             <h1 className="text-center auth-title">
               Aman Ullah
             </h1>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-
-              <Grid item xs={12}>
-                <Item>
-                  <div className="avatar">
-                    <div className="icon">
-                      <EditIcon
-                        viewBox="-8 -5 40 40"
-                      // fontSize="large" 
-                      />
-                    </div>
-                    <Avatar
-                      sx={{ width: 120, height: 120 }}
-                      alt="Remy Sharp" src="https://mui.com/static/images/avatar/2.jpg" />
-                  </div>
-                </Item>
-              </Grid>
-              <Grid item xs={6}>
-                <Item>
-                  <TextField
-                    fullWidth
-                    variant="outlined"
-                    id="name"
-                    name="name"
-                    label="Name"
-                    value={props.values.name}
-                    onChange={props.handleChange}
-                    error={
-                      props.touched.name && Boolean(props.errors.name)
-                    }
-                    helperText={props.touched.name && props.errors.name}
-                  />
-                </Item>
-              </Grid>
-              <Grid item xs={6}>
-                <Item>
-                  <TextField
-                    fullWidth
-                    type="number"
-                    variant="outlined"
-                    id="age"
-                    name="age"
-                    label="Age"
-                    value={props.values.age}
-                    onChange={props.handleChange}
-                    error={
-                      props.touched.age && Boolean(props.errors.age)
-                    }
-                    helperText={props.touched.age && props.errors.age}
-                  />
-                </Item>
-              </Grid>
-              <Grid item xs={12}>
-                <Item>
-                  <Typography variant="h5" gutterBottom component="div">
-                    Work experiences
-                  </Typography>
-                  <FieldArray
-                    name="workExperiences"
-                    render={arrayHelpers => (
-                      <div>
-                        {props.values.workExperiences && props.values.workExperiences.length > 0 ? (
-                          props.values.workExperiences.map((workExperience, index) => (
-                            <div key={index}>
-                              <TextField label="Start Date" variant="outlined"
-                                value={props.values.workExperiences[index].startDate}
-                                onChange={props.handleChange}
-                                error={
-                                  props.touched.age && Boolean(props.errors.age)
-                                }
-                                helperText={props.values.workExperiences[index].startDate && props.errors.workExperiences[index].startDate}
-                                name={`workExperiences.${index}.startDate`} />
-                              <TextField label="End Date" variant="outlined"
-                                value={props.values.workExperiences[index].endDate}
-                                onChange={props.handleChange}
-                                error={
-                                  props.touched.age && Boolean(props.errors.age)
-                                }
-                                helperText={props.values.workExperiences[index].endDate && props.errors.workExperiences[index].endDate}
-                                name={`workExperiences.${index}.endDate`} />
-                              <TextField label="Job title" variant="outlined"
-                                value={props.values.workExperiences[index].jobTitle}
-                                onChange={props.handleChange}
-                                error={
-                                  props.touched.age && Boolean(props.errors.age)
-                                }
-                                helperText={props.values.workExperiences[index].jobTitle && props.errors.workExperiences[index].jobTitle}
-                                name={`workExperiences.${index}.jobTitle`} />
-                              <TextField label="Company" variant="outlined"
-                                value={props.values.workExperiences[index].company}
-                                onChange={props.handleChange}
-                                error={
-                                  props.touched.age && Boolean(props.errors.age)
-                                }
-                                helperText={props.values.workExperiences[index].company && props.errors.workExperiences[index].company}
-                                name={`workExperiences.${index}.company`} />
+            <LocalizationProvider dateAdapter={AdapterMoment}>
 
 
-                              <TextField label="Job description" variant="outlined"
-                                value={props.values.workExperiences[index].jobDescription}
-                                onChange={props.handleChange}
-                                error={
-                                  props.touched.age && Boolean(props.errors.age)
-                                }
-                                helperText={props.values.workExperiences[index].jobDescription && props.errors.workExperiences[index].jobDescription}
-                                name={`workExperiences.${index}.jobDescription`} />
+              <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
 
-                              <input id={"file" + index}
-                                value={props.values.workExperiences[index].companyLogo} onChange={props.handleChange}
-                              />
-
-
-                              <Thumb file={props.values.workExperiences[index].companyLogo} />
-                              <button
-                                type="button"
-                                onClick={() => arrayHelpers.remove(index)} // remove a workExperience from the list
-                              >
-                                -
-                              </button>
-                              <button
-                                type="button"
-                                onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
-                              >
-                                +
-                              </button>
-                            </div>
-                          ))
-                        ) : (
-                          <Button type="button" onClick={() => arrayHelpers.push('')}>
-                            {/* show this when user has removed all workExperiences from the list */}
-                            Add a workExperience
-                          </Button>
-                        )}
-                        <div>
-                          <button type="submit">Submit</button>
-                        </div>
+                <Grid item xs={12}>
+                  <Item>
+                    <div className="avatar">
+                      <div className="icon">
+                        <EditIcon
+                          viewBox="-8 -5 40 40"
+                        // fontSize="large" 
+                        />
                       </div>
-                    )}
-                  />
+                      <Avatar
+                        sx={{ width: 120, height: 120 }}
+                        alt="Remy Sharp" src="https://mui.com/static/images/avatar/2.jpg" />
+                    </div>
+                  </Item>
+                </Grid>
+                <Grid item xs={6}>
+                  <Item>
+                    <TextField
+                      fullWidth
+                      variant="outlined"
+                      id="name"
+                      name="name"
+                      label="Name"
+                      value={props.values.name}
+                      onChange={props.handleChange}
+                      error={
+                        props.touched.name && Boolean(props.errors.name)
+                      }
+                      helperText={props.touched.name && props.errors.name}
+                    />
+                  </Item>
+                </Grid>
+                <Grid item xs={6}>
+                  <Item>
+                    <TextField
+                      fullWidth
+                      type="number"
+                      variant="outlined"
+                      id="age"
+                      name="age"
+                      label="Age"
+                      value={props.values.age}
+                      onChange={props.handleChange}
+                      error={
+                        props.touched.age && Boolean(props.errors.age)
+                      }
+                      helperText={props.touched.age && props.errors.age}
+                    />
+                  </Item>
+                </Grid>
+                <Grid item xs={12}>
+                  <Item>
+                    <Typography variant="h5" gutterBottom component="div">
+                      Work experiences
+                    </Typography>
+                    <FieldArray
+                      name="workExperiences"
+                      render={arrayHelpers => (
+                        <div>
+                          {props.values.workExperiences && props.values.workExperiences.length > 0 ? (
+                            props.values.workExperiences.map((workExperience, index) => (
+                              <div key={index}>
+                                <TextField label="Start Date" variant="outlined"
+                                  value={props.values.workExperiences[index].startDate}
+                                  onChange={props.handleChange}
+                                  error={
+                                    props.touched.age && Boolean(props.errors.age)
+                                  }
+                                  helperText={props.values.workExperiences[index].startDate && props.errors.workExperiences[index].startDate}
+                                  name={`workExperiences.${index}.startDate`} />
 
-                </Item>
-                <Item>
+                                <DesktopDatePicker
+                                  label="Date desktop"
+                                  inputFormat="MM/dd/yyyy"
+                                  value={value}
+                                  onChange={handleChange}
+                                  renderInput={(params) => <TextField {...params} />}
+                                />
+                                <TextField label="End Date" variant="outlined"
+                                  value={props.values.workExperiences[index].endDate}
+                                  onChange={props.handleChange}
+                                  error={
+                                    props.touched.age && Boolean(props.errors.age)
+                                  }
+                                  helperText={props.values.workExperiences[index].endDate && props.errors.workExperiences[index].endDate}
+                                  name={`workExperiences.${index}.endDate`} />
+                                <TextField label="Job title" variant="outlined"
+                                  value={props.values.workExperiences[index].jobTitle}
+                                  onChange={props.handleChange}
+                                  error={
+                                    props.touched.age && Boolean(props.errors.age)
+                                  }
+                                  helperText={props.values.workExperiences[index].jobTitle && props.errors.workExperiences[index].jobTitle}
+                                  name={`workExperiences.${index}.jobTitle`} />
+                                <TextField label="Company" variant="outlined"
+                                  value={props.values.workExperiences[index].company}
+                                  onChange={props.handleChange}
+                                  error={
+                                    props.touched.age && Boolean(props.errors.age)
+                                  }
+                                  helperText={props.values.workExperiences[index].company && props.errors.workExperiences[index].company}
+                                  name={`workExperiences.${index}.company`} />
 
-                </Item>
+
+                                <TextField label="Job description" variant="outlined"
+                                  value={props.values.workExperiences[index].jobDescription}
+                                  onChange={props.handleChange}
+                                  error={
+                                    props.touched.age && Boolean(props.errors.age)
+                                  }
+                                  helperText={props.values.workExperiences[index].jobDescription && props.errors.workExperiences[index].jobDescription}
+                                  name={`workExperiences.${index}.jobDescription`} />
+
+
+                                <input id={"file" + index}
+                                  name={`workExperiences.${index}.companyLogo`}
+                                  hidden
+                                  ref={companyLogoRef}
+                                  type="file" onChange={(event) => {
+                                    props.setFieldValue(`workExperiences.${index}.companyLogo`, event.currentTarget.files[0]);
+                                  }}
+                                />
+                                <Button variant="contained" color="primary" onClick={() => { triggerFileUpload() }}>
+                                  Company Logo
+                                </Button>
+
+
+                                <Thumb file={props.values.workExperiences[index].companyLogo} />
+                                <Button
+                                  variant="outlined"
+                                  onClick={() => arrayHelpers.remove(index)} // remove a workExperience from the list
+                                >
+                                  -
+                                </Button>
+                                <Button
+                                  variant="outlined"
+                                  onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
+                                >
+                                  +
+                                </Button>
+                              </div>
+                            ))
+                          ) : (
+                            <Button type="button" onClick={() => arrayHelpers.push('')}>
+                              {/* show this when user has removed all workExperiences from the list */}
+                              Add a workExperience
+                            </Button>
+                          )}
+                          <div>
+                            <Button variant="contained" className="mt-xs-20" type="submit">Submit</Button>
+                          </div>
+                        </div>
+                      )}
+                    />
+
+                  </Item>
+                  <Item>
+
+                  </Item>
+                </Grid>
+
               </Grid>
-
-            </Grid>
-
+            </LocalizationProvider>
           </form>
         )
       }
@@ -258,6 +292,7 @@ class Thumb extends React.Component {
   }
 
   render() {
+    console.log(this.props)
     const { file } = this.props;
     const { loading, thumb } = this.state;
 
